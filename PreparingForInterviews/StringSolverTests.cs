eb00b350505e4cd56a75356534870c7af3ff9f79
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-11 12:27:53 +0200

    Initial commit

---
+++ PreparingForInterviews/StringSolverTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MainClassLibrary;
+
+namespace PreparingForInterviews
+{
+    [TestClass]
+    public class StringSolverTests
+    {
+
+        AllUniqueCharSolver allUniqueCharSolver;
+
+        public StringSolverTests()
+        {
+            allUniqueCharSolver = new AllUniqueCharSolver();
+        }
+
+        [TestMethod]
+        public void CheckIfCharsAreUnique()
+        {
+            Assert.AreEqual(allUniqueCharSolver.Solve("Abcd"), true);
+        }
+
+        [TestMethod]
+        public void CheckIfCharsAreNotUnique()
+        {
+            Assert.AreEqual(allUniqueCharSolver.Solve("Testing"), true);
+        }
+
+
+
+        
+    }
+}
