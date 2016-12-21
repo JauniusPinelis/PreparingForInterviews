a5fb139cc9f7391ecdaddb314337d8bc6897e4ca
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-21 09:03:15 +0200

    Adding basic linked list

---
+++ MainClassLibrary/Data Structures/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClassLibrary.Data_Structures
+{
+    public class LinkedList
+    {
+        //Empty for now   
+    }
+
+    class Node
+    {
+        Node next = null;
+        int data;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+
+        void appendToTail(int d)
+        {
+            Node end = new Node(d);
+            Node n = this;
+            while (n.next != null)
+                n = n.next;
+            n.next = end;
+        }
+
+        Node deleteNode(Node head, int d)
+        {
+            Node n = head;
+            if (n.data == d)
+            {
+                return head.next;
+            }
+            while (n.next != null)
+            {
+                if (n.next.data == d)
+                {
+                    n.next = n.next.next;
+                    return head;
+                }
+                n = n.next;
+            }
+            return head; // d was not found - no changes
+        }
+    }
+}
